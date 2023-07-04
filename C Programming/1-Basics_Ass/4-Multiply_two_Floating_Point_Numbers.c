72de21560ce57d8227cfc0b274f5dd14c2826eb2
Rashad <rashadelgarf3@gmai.com>
2023-07-04 05:29:32 +0300

    updating

---
+++ C Programming/1-Basics_Ass/4-Multiply_two_Floating_Point_Numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    float x , y ;
+    float product =0;
+      printf("Enter two floats: ");
+      scanf("%f %f",&x,&y);
+      product =x*y;
+      printf("Product : %f ",product);  
+    return 0;
+}
---
+++ C Programming/1-Basics_Ass/5-Find_ASCII_Value_of_a_Character.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{   
+    char x;
+       printf("Enter a character: ");
+       scanf("%c",&x);
+       printf("ASCII value of %d", x);
+    return 0;
+}
