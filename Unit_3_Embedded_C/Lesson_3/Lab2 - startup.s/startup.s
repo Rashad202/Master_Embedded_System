ad6806a3a2119d1e56e6396765f5951141aca5b8
Rashad <Rashadelgarf3@gmail.com>
2023-07-15 16:34:03 +0300

    done

---
+++ Unit_3_Embedded_C/Lesson_3/Lab2 - startup.s/startup.s
@@ -0,0 +1,40 @@
+/* Startup 
+by:Rashad
+*/
+
+
+.section .vectors
+.word 0x20001000                /* Stack top Address --> Stack Pointer */
+.word _reset                    /* 1  reset handler */
+.word _VECTOR_handler 	    	/* 2  NMI */
+.word _VECTOR_handler 	    	/* 3  Hard Fault */
+.word _VECTOR_handler 	    	/* 4  MM Fault */
+.word _VECTOR_handler 	    	/* 5  Bus Fault */
+.word _VECTOR_handler 	    	/* 6  Usage Fault */
+.word _VECTOR_handler 	    	/* 7  RESERVED */
+.word _VECTOR_handler 	    	/* 8  RESERVED */
+.word _VECTOR_handler 	    	/* 9  RESERVED */
+.word _VECTOR_handler 	    	/* 10 RESERVED */
+.word _VECTOR_handler 	    	/* 11 SV Call */
+.word _VECTOR_handler 	    	/* 12 Debug Reserved */
+.word _VECTOR_handler 	    	/* 13 RESERVED */
+.word _VECTOR_handler 	    	/* 14 PendSV */
+.word _VECTOR_handler 	    	/* 15 SysTick */
+.word _VECTOR_handler 	    	/* 16 IRQ0 */
+.word _VECTOR_handler 	    	/* 17 IRQ1 */
+.word _VECTOR_handler 	    	/* 18 IRQ2 */
+.word _VECTOR_handler 	    	/* 19 ... */  
+
+.thumb_func
+
+.section .text
+_reset:
+	bl main
+	b .
+
+
+_VECTOR_handler:
+b _reset
+
+
+	
